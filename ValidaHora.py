e3df19c936aa4471d88e453205f3fdaf258ea982
Francimar Alexandre <92753716+AlexandreDNTS@users.noreply.github.com>
2023-02-28 17:17:27 -0300

    Add files via upload

---
+++ ValidaHora.py
@@ -0,0 +1,15 @@
+def ValidaHora(Tempo):
+    hora,minuto = map(int,Tempo.split(':'))
+
+    if minuto < 0 or minuto > 60 or hora < 0:
+        return False
+    elif minuto >= 0 and hora <= 0 or minuto == 0 and hora>0 or minuto > 0 and hora>0:
+        return True
+
+t = input("hora(hora:minuto ): ")
+tv = ValidaHora(t)
+
+if tv == True:
+    print('hora correta')
+elif tv== False:
+    print('hora incorreta')
