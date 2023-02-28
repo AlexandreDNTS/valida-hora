ce11ca993498f43bfe3af75aca82fc5614c7db8e
Francimar Alexandre <92753716+AlexandreDNTS@users.noreply.github.com>
2023-02-28 20:50:36 -0300

    atualizando as condições de validação

--- ValidaHora.py
+++ ValidaHora.py
@@ -1,11 +1,10 @@
 def ValidaHora(Tempo):
     hora,minuto = map(int,Tempo.split(':'))
 
-    if minuto < 0 or minuto > 60 or hora < 0:
+    if minuto < 0 or minuto > 59 or hora < 0 or hora >23:
         return False
     elif minuto >= 0 and hora <= 0 or minuto == 0 and hora>0 or minuto > 0 and hora>0:
         return True
-
 t = input("hora(hora:minuto ): ")
 tv = ValidaHora(t)
 
